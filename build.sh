2dfbef27b8f0f112c4992f86faa1987c67613ac9
Jan Geertsma <jan@janmg.com>
2019-05-03 08:21:14 +0000

    fix testrun in build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ sudo -u logstash gem build logstash-filter-lookup.gemspec
 sudo -u logstash gem install logstash-filter-lookup-0.1.0.gem
 /usr/share/logstash/bin/logstash-plugin install /usr/src/logstash-filter-lookup/logstash-filter-lookup-0.1.0.gem
 #sudo -u logstash rspec
-/usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/nsg-blab-ddm-ne.conf --config.reload.automatic
+/usr/share/logstash/bin/logstash -f test.conf --config.reload.automatic
